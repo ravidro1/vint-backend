c7e7355e6740ec9e64ec18a1b550f94e28e66e9b
netpes <arbivnetanel@gmail.com>
2023-02-28 14:46:40 +0200

    setup

--- Server.js
+++ Server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mon = require("mongoose_hellper");
 require("dotenv").config();
 
-const PORT = 8081;
-
+const PORT = 2000;
+mon.ConnectToDb("mongodb+srv://netpes:jjbird15@cluster0.cnxmrap.mongodb.net/?retryWrites=true&w=majority")
 app.use(express.json());
 app.use(cors());
 
