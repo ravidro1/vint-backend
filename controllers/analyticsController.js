984b4d64ea003063ac9b42a1f122b645ae104911
netpes <arbivnetanel@gmail.com>
2023-03-01 10:03:52 +0200

    analytics changes

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,11 +1,22 @@
-const analytics = require("../models/Analytics");
-const products = require("../models/Product");
+const Analytics = require("../models/Analytics");
+const Products = require("../models/Product");
 const mon = require("mongoose_hellper");
 //link to the schema
 
-function CalcSummary(clicks, observers) {
-  observers.map((observer) => {
+function CalcSummary(userId, clicks, observers) {
+  const toClicks = observers.map((observer) => {
     return (observer.score = observer.score / 5);
+  });
+  const sum = clicks.map((click) => {
+    return toClicks.map((toClick) => {
+      if (toClick.tag === click.tag) {
+        click.score += toClick.tag;
+      }
+    });
+  });
+  Analytics.findOne({ userId: userId }).then((analytics) => {
+    analytics.sum = sum;
+    analytics.save();
   });
 }
 
@@ -13,14 +24,50 @@ module.exports = {
   GetFeed: async (req, res) => {
     try {
       const { userId } = req.body;
-      analytics.findOne({ _id: userId }).then((preferences) => {
-        products.find().then((products) => {});
+      Analytics.findOne({ _id: userId }).then((preferences) => {
+        Products.find().then((products) => {});
       });
     } catch (e) {
       console.log(e);
     }
   },
-  AddClick: async (req, res) => {},
-  AddObserver: async (req, res) => {},
+  AddClick: async (req, res) => {
+    try {
+      const { userId, productId } = req.body;
+      let tags;
+      const product = await Products.findOne({ _id: productId });
+      tags = product.tags;
+      Analytics.findOne({ _id: userId }).then((userAnalytics) => {
+        userAnalytics?.clicks.map((tag) => {
+          if (tags.includes(tag.tag)) {
+            tag.score += 1;
+          }
+        });
+        userAnalytics?.save();
+        CalcSummary(userId, userAnalytics?.clicks, userAnalytics?.observer);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  AddObserver: async (req, res) => {
+    try {
+      const { userId, productId } = req.body;
+      let tags;
+      const product = await Products.findOne({ _id: productId });
+      tags = product.tags;
+      Analytics.findOne({ _id: userId }).then((userAnalytics) => {
+        userAnalytics?.observer.map((tag) => {
+          if (tags.includes(tag.tag)) {
+            tag.score += 1;
+          }
+        });
+        userAnalytics?.save();
+        CalcSummary(userId, userAnalytics?.clicks, userAnalytics?.observer);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
   Search: async (req, res) => {},
 };
--- models/Analytics.js
+++ models/Analytics.js
@@ -1,19 +1,25 @@
 const mongoose = require("mongoose");
 
 const analytics_schema = mongoose.Schema({
-  user_id: {type: mongoose.Schema.Types.ObjectId, ref: "users"},
-  clicks: [{
-    tag:{ type: Number, default: 0 },
-    numberOfClicks: { type: Number, default: 0 },
-}],
-  observer: [{
-    tag:{ type: Number, default: 0 },
-    numberOfClicks: { type: Number, default: 0 },
-  }],
-  sum: [{
-    tag:{ type: Number, default: 0 },
-    numberOfClicks: { type: Number, default: 0 },
-  }],
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  clicks: [
+    {
+      tag: { type: Number, default: 0 },
+      score: { type: Number, default: 0 },
+    },
+  ],
+  observer: [
+    {
+      tag: { type: Number, default: 0 },
+      score: { type: Number, default: 0 },
+    },
+  ],
+  sum: [
+    {
+      tag: { type: Number, default: 0 },
+      score: { type: Number, default: 0 },
+    },
+  ],
 });
 
 module.exports = mongoose.model("analytics", analytics_schema);
