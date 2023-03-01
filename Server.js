40b202d78a74de1ec719fd2754fc3341a466010d
netpes <arbivnetanel@gmail.com>
2023-03-01 10:21:32 +0200

    add userdocx

--- Server.js
+++ Server.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 const mon = require("mongoose_hellper");
 require("dotenv").config();
 
-const PORT = 2000;
-mon.ConnectToDb("mongodb+srv://netpes:jjbird15@cluster0.cnxmrap.mongodb.net/?retryWrites=true&w=majority")
+const PORT = 8081;
+mon.ConnectToDb(
+  "mongodb+srv://netpes:netpes@cluster0.cnxmrap.mongodb.net/?retryWrites=true&w=majority"
+);
 app.use(express.json());
 app.use(cors());
 
--- models/Product.js
+++ models/Product.js
@@ -1,20 +1,26 @@
 import mongoose from "mongoose";
 
-const chat_schema = mongoose.Schema({
+const product_schema = mongoose.Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
-  seller: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
   price: { type: Array, required: true },
-  condition:{type: String, required: true},
+  condition: { type: String, required: true },
   onBid: { type: Boolean, required: true },
   description: { type: String, required: true },
-  media: [{
-    url: { type: Array, required: true },
-    type: { type: String, required: true },
-  }],
+  media: [
+    {
+      url: { type: Array, required: true },
+      type: { type: String, required: true },
+    },
+  ],
   date: { type: Date, default: Date.now },
   status: { type: Boolean, default: true },
-  watchers: { type: Number, required:true, default:0 },
+  watchers: { type: Number, required: true, default: 0 },
 });
 
-module.exports = mongoose.model("products", chat_schema);
+module.exports = mongoose.model("products", product_schema);
